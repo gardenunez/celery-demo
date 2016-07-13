2e86d627f5806e031f5ff82fa352162914dd52f1
gardenunez <gardenunez@gmail.com>
2016-07-13 16:20:16 +0200

    create a CrawlTask class and add some handlers

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import proj.tasks as tasks
 from faker import Factory
 
 if __name__ == "__main__":
+    fake = Factory.create()
     tasks.split.delay(0,10)
 
-    fake = Factory.create()
-    for i in xrange(5):
+    for i in xrange(2):
         url = fake.url()
         tasks.crawl.apply_async((url,), countdown=i*2)
+
+    for i in xrange(2):
+        url = fake.url()
+        task = tasks.CrawlTask()
+        task.apply_async((url, ), countdown=i*2)
--- proj/tasks.py
+++ proj/tasks.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from proj.celery import app
 from celery.utils.log import get_task_logger
+from celery import Task
+from celery.exceptions import Reject
 import requests
 
 logger = get_task_logger(__name__)
@@ -30,7 +32,33 @@ def crawl(self, url):
         self.retry(url, exc=req_exc)
 
 
+class CrawlTask(Task):
 
+    def __init__(self):
+        self.max_retries = 3
+        self.default_retry_delay = 1
+        self.rate_limit = 1
+        self.name = 'proj.tasks.CrawlTask'
+        self.ignore_result = True
 
+    def run(self, url):
+        logger.info("START crawling {0}".format(url))
+        try:
+            r = requests.get(url)
+            logger.info("url:{0} -> status_code: {1} content-type:{2}".format(url, r.status_code, r.headers['content-type']))
+        except requests.exceptions.RequestException as req_exc:
+            logger.error("ERROR occur for {}".format(url))
+            self.retry(exc=req_exc)
 
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        logger.warn("Task: {} failed".format(self.name))
 
+    def on_retry(self, exc, task_id, args, kwargs, einfo):
+        logger.warn("Task: {} is retrying".format(self.name))
+
+    def on_success(self, retval, task_id, args, kwargs):
+        logger.info("Task: {} has finished".format(self.name))
+
+    def after_return(self, status, retval, task_id, args, kwargs, einfo):
+        if self.max_retries == self.request.retries or status == "FAILURE":
+            raise Reject(reason=einfo, requeue=False)
